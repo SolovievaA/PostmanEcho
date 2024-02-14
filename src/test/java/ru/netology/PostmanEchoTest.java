52bb611cf957c4697e54040baf63f600b95d5696
Alla QA <allushka181@gmail.com>
2024-02-14 18:31:15 +0300

    Change file

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -12,7 +12,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("File")
+                .body(text)
                 .when()
                 .post("/post")
                 .then()
